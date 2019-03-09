fa5e3d068d57d3a124e23a1be1de883a33452706
unkper <yangqingyang0428@163.com>
2019-03-09 11:11:13 +0800

    Update ExcelLoader.cs

--- Resource/UnityHelpCollection/Editor/RPG/ExcelLoader.cs
+++ Resource/UnityHelpCollection/Editor/RPG/ExcelLoader.cs
@@ -9,37 +9,39 @@ public class ExcelLoader
     [MenuItem("Excel/Equipment")]
     static void LoadEquip()
     {
-        using(FileStream f = new FileStream(Application.dataPath+ "shareArea1/Resource/Excel/equip.xlsx",FileMode.Open,FileAccess.Read))
+        using(FileStream f = new FileStream(Application.dataPath+ "/shareArea1/Resource/Excel/equip.xlsx",FileMode.Open,FileAccess.Read))
         {
             using(ExcelPackage pack = new ExcelPackage(f))
             {
                 var sheets = pack.Workbook.Worksheets;
-                ExcelWorksheet sheet = sheets[0];
+                ExcelWorksheet sheet = sheets[1];
                 int i;
-                i = 1;
-                while (sheet.Cells[i, 0].Text.Length!=0)
+                i = 2;
+                while (sheet.Cells[i, 1].Text.Length!=0)
                 {
-                    var itemID = sheet.Cells[i, 0].Text;
+                    var itemID = sheet.Cells[i, 1].Text;
                     var equip = AssetDatabase.LoadAssetAtPath<Equipment>(ScriptObjectCreator.pathBase + "EquipAsset/" + itemID + ".asset");
                     if (equip != null)
                     {
                         equip.ID = itemID;
-                        equip.name = sheet.Cells[i, 1].Text;
-                        for(int j = 2; j <= 9; j++)
+                        equip.name = sheet.Cells[i, 2].Text;
+                        for(int j = 3; j < 3 + Enum.GetNames(typeof(EquipType)).Length; j++)
                         {
                             int value = int.Parse(sheet.Cells[i, j].Text);
-                            equip.spawns.Add(new Equipment.SpawnEquip((EquipType)(j - 2), value));
+                            if(value!=0)
+                                equip.spawns.Add(new Equipment.SpawnEquip((EquipType)(j - 3), value));
                         }
                     }
                     else
                     {
                         equip = CreateScriptableObj();
                         equip.ID = itemID;
-                        equip.name = sheet.Cells[i, 1].Text;
-                        for (int j = 2; j <= 9; j++)
+                        equip.name = sheet.Cells[i, 2].Text;
+                        for (int j = 3; j < 3 + Enum.GetNames(typeof(EquipType)).Length; j++)
                         {
                             int value = int.Parse(sheet.Cells[i, j].Text);
-                            equip.spawns.Add(new Equipment.SpawnEquip((EquipType)(j - 2), value));
+                            if(value!=0)
+                                equip.spawns.Add(new Equipment.SpawnEquip((EquipType)(j - 3), value));
                         }
                     }
                     SaveObjData(equip, equip.ID, "/EquipAsset/");
@@ -57,14 +59,13 @@ public class ExcelLoader
     {
         FileInfo f = new FileInfo(Application.dataPath + "/shareArea1/Resource/Excel/equip.xlsx");
         ExcelPackage package = new ExcelPackage(f);
-        var sheet = package.Workbook.Worksheets.Add("Sheet1");
+        var sheet = package.Workbook.Worksheets.Add("装备属性表");
         sheet.Cells[1, 1].Value = "ID";
         sheet.Cells[1, 2].Value = "Name";
         int i = 3;
         foreach(var s in Enum.GetNames(typeof(EquipType)))
             sheet.Cells[1, i++].Value = s;
-        package.Save();
-        
+        package.Save();  
     }
 
     static Equipment CreateScriptableObj()
