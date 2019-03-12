b71ef9a89cf86ee721726cfeababea577ea0f23c
unkper <33563150+unkper@users.noreply.github.com>
2019-03-12 23:55:51 +0800

    newUpdate1

---
+++ Resource/UnityHelpCollection/Runtime/RPG/ModelInterface.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine.Events;
+
+
+public interface ModelInterface
+{
+    event Action<float> HpChanged;
+    event Action<float> MpChanged;
+    event Action<int,string> SkillChanged;
+    event Action<int> LevelChanged;
+    event Action<float> ExperienceChanged;
+    event Action<int, string> EquipChanged;
+    event Action<int, int, string> PackageChanged;
+    void SetSkill(int slotNumber, string skillID);
+    void SetEquip(int EquipType, string equipID);
+    void SetPackage(int row, int line, string equipID);
+}
+
