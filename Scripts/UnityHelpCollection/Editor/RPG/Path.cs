f4023a8c91217db4eb3d33a2dbe6bd1e089a1f96
unkper <yangqingyang0428@163.com>
2019-03-15 12:50:25 +0800

    2

---
+++ Scripts/UnityHelpCollection/Editor/RPG/Path.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class m_Path
+{
+    public static string pathBase = "Assets/shareArea1/Resource/";
+    public static string pPicture = pathBase + "Source/Photo/";
+    public static string pPicEquip = pPicture + "Equip/";
+    public static string pPicSkill = pPicture + "Skill/";
+    public static string pData = pathBase + "Data/";
+    public static string pDataEquip = pData + "Equip/";
+    public static string pDataSkill = pData + "Skill/";
+
+    public static string resPathBase = "shareArea1/";
+    public static string respPicture = resPathBase + "Source/Photo/";
+    public static string respPicEquip = respPicture + "Equip/";
+    public static string respPicSkill = respPicture + "Skill/";
+    public static string respData = resPathBase + "Data/";
+    public static string respDataEquip = respPicture + "Equip/";
+    public static string respDataSkill = respPicture + "Skill/";
+}
