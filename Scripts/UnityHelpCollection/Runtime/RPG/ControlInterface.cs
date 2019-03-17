d12c4863633b3a6d054deecd8b7a9d73c6aa1392
unkper <33563150+unkper@users.noreply.github.com>
2019-03-17 11:39:40 +0800

    1223

---
+++ Scripts/UnityHelpCollection/Runtime/RPG/ControlInterface.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public interface ControlInterface
+{
+    void TabBtnClick(Button button);
+    void PackageBtnClick(Button button);
+    void EquipBtnClick(Button button);
+}
+
